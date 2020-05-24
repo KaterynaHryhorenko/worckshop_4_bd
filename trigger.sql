238133f355d4941f102e854e6cc7f2d50afad8d8
григоренко катерина <mamalaga63@gmail.com>
2020-05-24 14:48:31 +0300

    commit

--- trigger.sql
+++ trigger.sql
@@ -1,16 +1,18 @@
 SET SERVEROUTPUT ON
 
 create or replace TRIGGER ban
-    before update of native_country1 on person
+    before update of native_country on person
     for each row
    
     declare
-    NATIVE_COUNTRY person.native_country1%type;
+    NATIVE_COUNTRY person.native_country%type;
     begin
-     if (:new.native_country1 != :old.native_country1) then
-     NATIVE_COUNTRY:= :old.native_country1;
-       dbms_output.put_line("Can't change native country");
+     if (:new.native_country != :old.native_country) then
+     NATIVE_COUNTRY:= :old.native_country;
+     :new.native_country := NATIVE_COUNTRY;
+       dbms_output.put_line('Can not change native country');
      end if;
     end ban;
     
-UPDATE person set native_country1 = 'Japan' where name = 'Yama Bob';
+--try    
+UPDATE person set native_country = 'Spain' where person_name = 'Jun Huan';
