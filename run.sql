b9de8122bdf7a47d5984758f7d0ff64da5358bee
григоренко катерина <mamalaga63@gmail.com>
2020-05-24 14:40:01 +0300

    commit

--- run.sql
+++ run.sql
@@ -2,6 +2,7 @@ declare
 passport_proc  person.passport%TYPE;
 country_in_proc country.country%TYPE;
 country_out_proc country.country%TYPE;
+date_migration_proc migrations.date_migration%type;
 cursor traveler_table
 is
 select
@@ -30,20 +31,25 @@ Check_migration(
 'Japan',
  'PJ123',
  'Italy',
-  to_date('2001/07/22', 'yyyy/mm/dd'),
- to_date('2001/11/11', 'yyyy/mm/dd')
+  to_date('2001/07/22', 'yyyy/mm/dd'),--new
+ to_date('2001/11/29', 'yyyy/mm/dd')--old
  ); 
  -- if unique constraint fails
 Check_migration(
 'Japan',
  'PJ123',
  'Italy',
-  to_date('2001/07/22', 'yyyy/mm/dd'),
- to_date('2001/11/11', 'yyyy/mm/dd')
+  to_date('2001/07/22', 'yyyy/mm/dd'),--new
+ to_date('2001/11/29', 'yyyy/mm/dd') --old
  );
- 
-  for rec in traveler_table 
-loop
-dbms_output.put_line(rec.person_name || rec.date_migration);
-end loop;
+
+select 
+ person_passport,
+ country_in, 
+ country_out,
+ date_migration
+into  passport_proc,country_in_proc, country_out_proc,date_migration_proc 
+from migrations
+where date_migration = to_date('2001/07/22', 'yyyy/mm/dd');
+dbms_output.put_line( passport_proc || country_in_proc || country_out_proc || date_migration_proc);
 end;
